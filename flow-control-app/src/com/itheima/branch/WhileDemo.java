314a059efa2519fb9bc6a9a549866273d623e753
cyandT <1821946709@qq.com>
2022-03-10 22:02:26 +0800

    while study

---
+++ flow-control-app/src/com/itheima/branch/WhileDemo.java
@@ -0,0 +1,15 @@
+package com.itheima.branch;
+
+public class WhileDemo {
+    public static void main(String[] args) {
+        double peakHeight = 8848860;
+        double paperThickness = 0.1;
+        int i = 0;
+        while (paperThickness < peakHeight){
+            paperThickness*=2;
+            i++;
+        }
+        System.out.println(i);
+        System.out.println(paperThickness);
+    }
+}
