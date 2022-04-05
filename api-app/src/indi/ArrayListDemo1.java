e3a4eb39e50197a95bae08aadf555c7ebc84c1db
cyandT <1821946709@qq.com>
2022-04-05 18:18:32 +0800

    String

---
+++ api-app/src/indi/ArrayListDemo1.java
@@ -0,0 +1,21 @@
+package indi;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo1 {
+     public static void main(String[] args) {
+          ArrayList<String> list = new ArrayList<>();
+          list.add("重返帝国");
+          list.add("之");
+          list.add("大败");
+          list.add("yyt");
+          list.add(1,"清幽");
+          System.out.println(list);
+
+          ArrayList<Integer> list1 = new ArrayList<>();
+          list1.add(5);
+          list1.add(20);
+          list1.add(1,4);
+          System.out.println(list1);
+     }
+}
